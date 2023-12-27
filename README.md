0f75e7b9f1485d59f8c509ffc8cc3e0685d75250
Nilton Manjate <103423713+nilton-manjate@users.noreply.github.com>
2023-12-27 12:36:27 +0200

    Upload all files

---
+++ README.md
@@ -0,0 +1,45 @@
+﻿# Frontend Mentor - Advice generator app
+
+![Design preview for the Advice generator app coding challenge](./design/desktop-preview.jpg)
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Generate a new piece of advice by clicking the dice icon
+
+### Links
+
+- Solution URL: [GitHub Repo](https://github.com/nilton-manjate/advice-generator)
+- Live Site URL: [GitHub Pages](https://nilton-manjate.github.io/advice-generator)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Vanilla JavaScript
+- Mobile-first workflow
+
+## Author
+
+- GitHub - [Nilton Manjate](https://github.com/nilton-manjate)
+
+- Frontend Mentor - [@nilton-manjate](https://www.frontendmentor.io/profile/nilton-manjate)
+
+- Twitter - [@maxatine](https://www.twitter.com/maxatine)
---
+++ script.js
@@ -0,0 +1,63 @@
+﻿"use strict";
+
+/* Declaring variables */
+const text = document.getElementsByTagName("p")[0];
+const dice = document.getElementsByClassName("dice")[0];
+const num = document.getElementsByTagName("span")[0];
+
+dice.addEventListener("click", function () {
+const firstText = `"It is easy to sit up and take notice, \
+                   what's difficult is getting up and \
+                   taking action."`;
+
+  switch (num.textContent) {
+    case "117":
+      num.textContent = "118";
+      text.textContent = `"All our dreams can come true, \
+                         if we have the courage to pursue them."`;
+      break;
+
+    case "118":
+      num.textContent = "119";
+      text.textContent = `"The secret of getting \
+      ahead is getting started." ― Mark Twain`;
+      break;
+
+    case "119":
+      num.textContent = "120";
+      text.textContent = `"The best time to plant a tree was 20 \
+                         years ago. The second best time is now."`;
+      break;
+
+    case "120":
+      num.textContent = "121";
+      text.textContent = `"It’s hard to beat a person who \
+                         never gives up". ― Babe Ruth`;
+      break;
+
+    case "121":
+      num.textContent = "122";
+      text.textContent = `"If people are doubting how far you can \
+                         go, go so far that you can’t hear them \
+                         anymore." ― Michele Ruiz`;
+      break;
+
+    case "122":
+      num.textContent = "123";
+      text.textContent = `"Everything you can imagine is real." \
+                         ― Pablo Picasso`;
+      break;
+
+    case "123":
+      num.textContent = "124";
+      text.textContent = `"Do one thing every day that scares you." \
+                         ― Eleanor Roosevelt`;
+      break;
+
+    case "124":
+      num.textContent = "117";
+      text.textContent = firstText;
+      break;
+  }
+
+});
